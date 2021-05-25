d5cec55bbcd0fd46a2b95bad2fa6c6015f535020
paul cedeño <pfcedeno.mdat@uisek.edu.ec>
2021-05-25 18:37:15 -0500

    renombre de index.py a main.py

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
-import sys
-from typing import ContextManager
-from flask import Flask, render_template
+from app.main import app
 
-from utils import get_industrias, get_model
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', ])
-def home():
-    
-    test_data = get_model()
-    industrias = get_industrias()
-
-    
-    return render_template('index.html', test_data=test_data, industrias=industrias)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    #app.run(debug=True, use_reloader=True)
+if __name__ == "__main__":
+  app.run()
