5aa375689ac548ef063f01f0beb92e897076ea5b
paul cedeño <pfcedeno.mdat@uisek.edu.ec>
2021-05-27 12:37:22 -0500

    redibujado de graficos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from typing import ContextManager
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from utils import get_industrias, get_model
 
@@ -8,6 +8,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', ])
 def home():
-    test_data = get_model()
+    return render_template('index.html')
+
+
+@app.route('/graficas', methods=['GET','POST'])
+def graficas():
     industrias = get_industrias()
-    return render_template('index.html', test_data=test_data, industrias=industrias)
+    #if request.method=='POST':
+    test_data = get_model()
+    return render_template('graficas.html', test_data=test_data, industrias=industrias)
