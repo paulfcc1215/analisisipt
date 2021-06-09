9fc4f2a504ff73b2492a01f695f83da3e62f619b
paulfcc1215 <paul.fcc@hotmail.com>
2021-06-09 03:29:21 -0500

    Se incluye funcion get_model

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from typing import ContextManager
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, make_response, request, json
 
-from utils import get_industrias, get_model
+from utils import *
 
 app = Flask(__name__)
 
@@ -14,6 +14,29 @@ def home():
 @app.route('/graficas', methods=['GET','POST'])
 def graficas():
     industrias = get_industrias()
-    #if request.method=='POST':
-    test_data = get_model()
+    test_data = test()
     return render_template('graficas.html', test_data=test_data, industrias=industrias)
+
+
+@app.route('/ajax', methods=['POST', ])
+def ajax_test():
+    
+    desde = request.form.get("fecha_desde")
+    hasta = request.form.get("fecha_hasta")
+    industrias = json.loads(request.form.get("industrias"))
+    print(industrias)
+    #data = get_model(5,desde,hasta)
+    for i in industrias:
+        data = get_model(i,desde,hasta)
+    
+    # data = {
+    #     'Feb-2016' : 93.82, 
+    #     'Mar-2016' : 93.82,
+    #     'Abr-2016' : 93.05,
+    #     'May-2016' : 93.45,
+    #     'Jun-2016' : 92.54,
+    #     'Jul-2016' : 92.20
+    # }
+    response = make_response(jsonify(data), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
--- utils.py
+++ utils.py
@@ -5,36 +5,52 @@ import pandas as pd
 # libreria numérica
 import numpy as np 
 # para crear gráficos con matplotlib
-#import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt 
 import plotly.express as px
 from datetime import datetime
 #test de addfuller
-#from statsmodels.tsa.stattools import adfuller
-#from pmdarima.arima import auto_arima
+from statsmodels.tsa.stattools import adfuller
+from pmdarima.arima import auto_arima
 import warnings
+
+from werkzeug import formparser
 warnings.filterwarnings('ignore')
-#from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima_model import ARIMA
 
-def get_model():
+def get_model(id_i,fd,fh):
 
-    ###importar dataset csv
-    # datos = pd.read_csv("data/ipt_2016_2021_4.csv", delimiter=";")
-    # datos = datos.set_index('PERIODO')
-    # datos.sort_index(inplace=True)
-    # catalogo_industrias = datos.keys()
-    # industria = catalogo_industrias[0]
+    res = {}
+    ipt_datos = {}
+    prediccion = {}
+    ##importar dataset csv
+    datos = pd.read_csv("data/ipt_2016_2021_4.csv", delimiter=";")
+    datos = datos.set_index('PERIODO')
+    datos.sort_index(inplace=True)
+    
+    industria = list(datos.keys())[int(id_i)]
+    
+    fechas = list(datos[industria].keys())
+    valores = datos[industria].values.tolist()
+    length = len(fechas)
+    for i in range(length):
+        ipt_datos[fechas[int(i)]] = valores[i]
+    
+    res['ipt_datos'] = ipt_datos
+    res['prediccion'] = prediccion
+    
+    return res
+    
+    # ##gráfica de dataset
+    # datos[industria].plot(figsize=(15,5))
+    # plt.title(industria)
+    # plt.xlabel('Serie de tiempo (PERIODO)')
+    # plt.ylabel('IPT')
+    # plt.show()
 
-    ###gráfica de dataset
-    #datos[industria].plot(figsize=(15,5))
-    #plt.title(industria)
-    #plt.xlabel('Serie de tiempo (PERIODO)')
-    #plt.ylabel('IPT')
-    #plt.show()
+    # datos_anual = datos.resample(rule='A').sum()
+    # datos_anual[industria].plot(figsize=(20,5))
 
-    #datos_anual = datos.resample(rule='A').sum()
-    #datos_anual[industria].plot(figsize=(20,5))
-
-    ###prueba unitaria a la serie de tiempo para obtener correlación
+    # ##prueba unitaria a la serie de tiempo para obtener correlación
     # def adfuller_test(dataset):
     #     dftest = adfuller(dataset,autolag='AIC')
     #     print("1. ADF: ", dftest[0])
@@ -84,11 +100,40 @@ def get_model():
         'Jul-2016' : 92.20
     }
 
+
+
+
+
+
+
+
+"""
+Funcion para obtener las industrias cargadas en la base
+"""
 def get_industrias():
     datos = pd.read_csv("data/ipt_2016_2021_4.csv", delimiter=";")
     datos = datos.set_index('PERIODO')
     catalogo_industrias = {}
+    i = 0
     for clave in datos.keys():
-        catalogo_industrias[clave]=clave
-
+        catalogo_industrias[i]=clave
+        i = i + 1
     return catalogo_industrias
+
+
+
+
+
+
+"""
+T e s t
+"""
+def test():
+    return {
+        'Feb-2016' : 93.82, 
+        'Mar-2016' : 93.82,
+        'Abr-2016' : 93.05,
+        'May-2016' : 93.45,
+        'Jun-2016' : 92.54,
+        'Jul-2016' : 92.20
+    }
